e33867d17e92122c79a93b9e49054f0bdfebf164
Urszula Dziewit <urszula.dziewit@inwenta.pl>
2017-06-10 14:35:25 +0200

    init

---
+++ js/zmienne.js
@@ -0,0 +1,22 @@
+"use strict"
+
+/* operatory arytmetyczne */
+var licz = 0;
+licz+= 1;
+console.log(licz);
+
+
+/* operatory porównania start */
+
+
+/* operatory porównania stop */
+
+(2>5) ? console.log("tak") : console.log("nie");
+
+
+if (2>5) {
+	console.log("tak")
+} else {
+	console.log("nie");
+	
+}
